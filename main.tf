4dc3630660388ea7198606f85673758e940e3023
hello-world-5 <hello-world-5@unknown.home>
2025-05-21 14:05:13 +0100

    First terraform init working directory

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Create EC2 instance
+provider "aws" {
+  region = "eu-west-1"
+}
+# Cloud provider (AWS)
+# Region
+# Resource
+# AMI ID ami-0c1c30571d2dae5c9 (for Ubuntu 22.04)
+# Which instance type(t3.micro)
+# Network information (public IP)
+# Name instance
+
+# HCL syntax is {key: value}
+# Never hardcode secret or access keys in here.
